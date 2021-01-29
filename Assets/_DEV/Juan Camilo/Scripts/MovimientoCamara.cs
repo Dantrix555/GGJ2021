4d845849725945cb9ef943a4a60aa21dbbd76a4a
jgamezc <60823075+jgamezc@users.noreply.github.com>
2021-01-28 23:09:36 -0500

    Movimiento y Camara
    Sistema de movimiento y seguimiento de cámara pulidos

--- Assets/_DEV/Juan Camilo/Scripts/MovimientoCamara.cs
+++ Assets/_DEV/Juan Camilo/Scripts/MovimientoCamara.cs
@@ -5,10 +5,22 @@ using UnityEngine;
 public class MovimientoCamara : MonoBehaviour
 {
     public Transform objetivo;
+    public Transform camara;
+    public float suavizado;
     Vector3 posicionFinal;
+    Vector3 velocidad = Vector3.zero;
 
+    private void Awake()
+    {
+        camara = transform.GetChild(0);
+    }
     private void Update()
     {
-        posicionFinal = Vector3.
+        posicionFinal = Vector3.SmoothDamp(transform.position, objetivo.position, ref velocidad, suavizado);
+    }
+    private void LateUpdate()
+    {
+        transform.position = posicionFinal;
+        camara.LookAt(objetivo);
     }
 }
